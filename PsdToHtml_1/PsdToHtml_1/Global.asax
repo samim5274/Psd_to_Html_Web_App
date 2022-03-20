190b380eb8d487d9cabd9d4ff61aad99aeb1b8c3
Samim Hossen <banglarcele1122@gmail.com>
2022-03-20 13:16:30 +0600

    up

---
+++ PsdToHtml_1/PsdToHtml_1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PsdToHtml_1.MvcApplication" Language="C#" %>
---
+++ PsdToHtml_1/PsdToHtml_1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PsdToHtml_1.Startup))]
+namespace PsdToHtml_1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
